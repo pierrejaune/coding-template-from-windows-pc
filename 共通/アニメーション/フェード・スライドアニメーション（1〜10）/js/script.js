dec3cb6aa7f2c71fcf5b33571009bf892868c802
yuji <53387749+pierrejaune@users.noreply.github.com>
2025-11-22 16:46:56 +0900

    重複ファイル削除

--- 共通/アニメーション/フェード・スライドアニメーション（1〜10）/js/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-$(document).ready(function () {
-  $('.animate-btn').click(function () {
-    var animationClass = $(this).data('animation');
-    var target = $('.animation-box');
-
-    target.removeClass().addClass('animation-box ' + animationClass);
-  });
-});
